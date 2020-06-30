1e3c02d2edb598f34d8ffd7bf848d665c4484047
vishnushashank43 <vishnushashank43@gmail.com>
2020-06-30 20:31:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -29,17 +29,39 @@ X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size = 0.2, rand
 X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
+mport pandas as pd
+from sklearn.preprocessing import LabelEncoder 
+from sklearn.model_selection import train_test_split 
+import pickle
+#importing
+df = pd.read_csv('insurance.csv')
+df.head()
+ #variables
+X = X = df.drop(columns='charges').values
+y = df['charges'].values
+#Labelencoding
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_4 = LabelEncoder()
+X[:, 4] = labelencoder_X_4.fit_transform(X[:, 4])
+labelencoder_X = LabelEncoder()
+X[:, 5] = labelencoder_X.fit_transform(X[:, 5])
+
+
+# train test split
+X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
 # buliding model
-regressor = xgb.XGBRegressor(n_estimators=100)
-                    
-regressor.fit(X_train,y_train)
-
-y_pred= regressor.predict(X_test)
+X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
+# buliding model
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
 
-pickle.dump(regressor , open('model.pkl','wb'))
+#Fitting model with trainig data
+regressor.fit(X_train, y_train)
 
-# Loading model to compare the results
-#model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
+# Saving model to disk
+pickle.dump(regressor, open('model.pkl','wb'))
