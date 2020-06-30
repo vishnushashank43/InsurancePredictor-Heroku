c27448fbf14fc25b24fa9c12591fb031df9ed367
vishnushashank43 <vishnushashank43@gmail.com>
2020-06-30 20:33:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 import numpy as np 
-import xgboost as xgb
 from sklearn.preprocessing import LabelEncoder 
 from sklearn.model_selection import train_test_split 
 import pickle
